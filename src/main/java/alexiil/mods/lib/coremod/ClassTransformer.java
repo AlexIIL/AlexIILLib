406ba88681c0ca64e7062ae1179538ff2bee010a
AlexIIL <alexj9@me.com>
2015-04-07 10:35:03 +0100

    Remove CivCraft class transforming

--- src/main/java/alexiil/mods/lib/coremod/ClassTransformer.java
+++ src/main/java/alexiil/mods/lib/coremod/ClassTransformer.java
@@ -25,16 +25,6 @@ import alexiil.mods.lib.Lib;
 
 public class ClassTransformer implements IClassTransformer {
     public static Logger log = LogManager.getLogger(Lib.Mod.ID + ".classTransformer");
-    public static final boolean civcraft;
-
-    static {
-        Class<?> cls = null;
-        try {
-            cls = Class.forName("alexiil.mods.civ.CivCraft", false, ClassTransformer.class.getClassLoader());
-        }
-        catch (ClassNotFoundException e) {}
-        civcraft = cls != null;
-    }
 
     @Override
     public byte[] transform(String name, String transformedName, byte[] basicClass) {
@@ -50,19 +40,6 @@ public class ClassTransformer implements IClassTransformer {
 
             if (transformedName.equals("net.minecraft.client.gui.GuiNewChat") && AlexIILLib.timeText.getBoolean())
                 return transformGuiNewChat(basicClass, !name.equals(transformedName));
-
-            // CivCraft Transforming
-            if (civcraft) {
-                if (transformedName.equals("net.minecraft.inventory.ContainerPlayer"))
-                    return transformContainerPlayer(basicClass, !name.equals(transformedName));
-
-                if (transformedName.equals("net.minecraft.inventory.ContainerWorkbench"))
-                    return transformContainerWorkbench(basicClass, !name.equals(transformedName));
-
-                if (transformedName.equals("net.minecraft.tileentity.TileEntityFurnace"))
-                    return transformTileEntityFurnace(basicClass, !name.equals(transformedName));
-            }
-
         }
         catch (Throwable t) {
             log.warn("Transforming class " + transformedName + " FAILIED! Returning the old version of the class to avoid crashes.");
@@ -71,7 +48,7 @@ public class ClassTransformer implements IClassTransformer {
         return basicClass;
     }
 
-    private void showDiff(String className, byte[] input, byte[] output) {
+    protected void showDiff(String className, byte[] input, byte[] output) {
         log.info("Finished Transforming " + className);
         if (input == null)
             log.debug("input was null");
@@ -305,174 +282,14 @@ public class ClassTransformer implements IClassTransformer {
         return out;
     }
 
-    private byte[] transformContainerPlayer(byte[] input, boolean obfuscated) {
-        String targetMethodName = obfuscated ? "func_75130_a" : "onCraftMatrixChanged";
-
-        ClassNode classNode = new ClassNode();
-        ClassReader reader = new ClassReader(input);
-        reader.accept(classNode, 0);
-        boolean found = false;
-
-        for (MethodNode m : classNode.methods) {
-            if (m.name.equals(targetMethodName)) {
-                found = true;
-                int ins = 0;
-                for (int i = 0; i < m.instructions.size(); i++) {
-                    if (m.instructions.get(i).getOpcode() == Opcodes.INVOKESTATIC) {
-                        m.instructions.remove(m.instructions.get(i));
-                        ins = i;
-                        break;
-                    }
-                }
-                ins += 4;
-                m.instructions.remove(m.instructions.get(ins));
-                m.instructions.remove(m.instructions.get(ins));
-                m.instructions.insert(m.instructions.get(ins - 1), new MethodInsnNode(Opcodes.INVOKESTATIC,
-                        "alexiil/mods/civ/event/VanillaEventHooks", "canCraftPlayerEvent",
-                        "(Lnet/minecraft/inventory/InventoryCrafting;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;",
-                        false));
-            }
-        }
-        if (!found) {
-            log.warn("Didn't find the method " + targetMethodName + " in ContainerPlayer!");
-            log.info("However, these methods exist, is it one of these?");
-            for (MethodNode m : classNode.methods) {
-                log.info("  -" + m.name + " with desc " + m.desc);
-            }
-        }
-
-        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
-        classNode.accept(cw);
-        byte[] out = cw.toByteArray();
-        showDiff("net.minecraft.inventory.ContainerPlayer", input, out);
-        return out;
-    }
-
-    private byte[] transformContainerWorkbench(byte[] input, boolean obfuscated) {
-        String targetMethodName = obfuscated ? "func_75130_a" : "onCraftMatrixChanged";
-
-        ClassNode classNode = new ClassNode();
-        ClassReader reader = new ClassReader(input);
-        reader.accept(classNode, 0);
-        boolean found = false;
-
-        for (MethodNode m : classNode.methods) {
-            if (m.name.equals(targetMethodName)) {
-                found = true;
-                int ins = 0;
-                for (int i = 0; i < m.instructions.size(); i++) {
-                    if (m.instructions.get(i).getOpcode() == Opcodes.INVOKESTATIC) {
-                        m.instructions.remove(m.instructions.get(i));
-                        ins = i;
-                        break;
-                    }
-                }
-
-                ins += 4;
-                m.instructions.remove(m.instructions.get(ins));
-                ins--;
-                m.instructions.insert(m.instructions.get(ins), new VarInsnNode(Opcodes.ALOAD, 0));
-                ins++;
-                m.instructions.insert(m.instructions.get(ins), new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/inventory/ContainerWorkbench",
-                        "field_178145_h", "Lnet/minecraft/util/BlockPos;"));
-                ins++;
-
-                String owner = "alexiil/mods/civ/event/VanillaEventHooks";
-                String desc = "(Lnet/minecraft/inventory/InventoryCrafting;Lnet/minecraft/world/World;Lnet/minecraft/util/BlockPos;)";
-                String desc2 = "Lnet/minecraft/item/ItemStack;";
-                MethodInsnNode node = new MethodInsnNode(Opcodes.INVOKESTATIC, owner, "canCraftBlockEvent", desc + desc2, false);
-                m.instructions.insert(m.instructions.get(ins), node);
-            }
-        }
-        if (!found) {
-            log.warn("Didn't find the method " + targetMethodName + " in ContainerWorkbench!");
-            log.info("However, these methods exist, is it one of these?");
-            for (MethodNode m : classNode.methods) {
-                log.info("  -" + m.name + " with desc " + m.desc);
-            }
-        }
-
-        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
-        classNode.accept(cw);
-        byte[] out = cw.toByteArray();
-        showDiff("net.minecraft.inventory.ContainerWorkbench", input, out);
-        return out;
-    }
-
-    private byte[] transformTileEntityFurnace(byte[] input, boolean obfuscated) {
-        String targetMethodName = obfuscated ? "func_145948_k" : "canSmelt";
-
-        ClassNode classNode = new ClassNode();
-        ClassReader reader = new ClassReader(input);
-        reader.accept(classNode, 0);
-        boolean found = false;
-
-        for (MethodNode m : classNode.methods) {
-            if (m.name.equals(targetMethodName)) {
-                found = true;
-                int ins = 0;
-                for (int i = 0; i < m.instructions.size(); i++) {
-                    if (m.instructions.get(i).getOpcode() == Opcodes.INVOKESTATIC) {
-                        m.instructions.remove(m.instructions.get(i));
-                        ins = i;
-                        break;
-                    }
-                }
-
-                ins += 4;
-
-                m.instructions.remove(m.instructions.get(ins));
-                ins--;
-
-                m.instructions.insert(m.instructions.get(ins), new VarInsnNode(Opcodes.ALOAD, 0));
-                ins++;
-
-                String worldObj = obfuscated ? "field_145850_b" : "worldObj";
-
-                m.instructions.insert(m.instructions.get(ins), new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/tileentity/TileEntityFurnace",
-                        worldObj, "Lnet/minecraft/world/World;"));
-                ins++;
-
-                m.instructions.insert(m.instructions.get(ins), new VarInsnNode(Opcodes.ALOAD, 0));
-                ins++;
-
-                String pos = obfuscated ? "field_174879_c" : "pos";
-
-                m.instructions.insert(m.instructions.get(ins), new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/tileentity/TileEntityFurnace", pos,
-                        "Lnet/minecraft/util/BlockPos;"));
-                ins++;
-
-                m.instructions.insert(m.instructions.get(ins), new MethodInsnNode(Opcodes.INVOKESTATIC, "alexiil/mods/civ/event/VanillaEventHooks",
-                        "canSmeltEvent",
-                        "(Lnet/minecraft/item/ItemStack;Lnet/minecraft/world/World;Lnet/minecraft/util/BlockPos;)Lnet/minecraft/item/ItemStack;",
-                        false));
-            }
-        }
-        if (!found) {
-            log.warn("Didn't find the method " + targetMethodName + " in TileEntityFurnace !");
-            log.info("However, these methods exist, is it one of these?");
-            for (MethodNode m : classNode.methods) {
-                log.info("  -" + m.name + " with desc " + m.desc);
-            }
-        }
-
-        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
-        classNode.accept(cw);
-        byte[] out = cw.toByteArray();
-        showDiff("net.minecraft.inventory.ContainerWorkbench", input, out);
-        return out;
-    }
-
-    @SuppressWarnings("unused")
-    private void showMethod(MethodNode m, int from) {
+    protected void showMethod(MethodNode m, int from) {
         log.info("Showing Method...");
         for (int i = from; i < m.instructions.size() && i < from + 12; i++) {
             log.info("  -" + getInsn(m.instructions.get(i)));
         }
     }
 
-    @SuppressWarnings("unused")
-    private void showMethodDiff(ClassNode node1, ClassNode node2, String methodName) {
+    protected void showMethodDiff(ClassNode node1, ClassNode node2, String methodName) {
         log.warn("---------------------------------------------------------------------------------------------------");
         log.warn("Showing Method Diffs of " + methodName + "----------------------------------------------------------------------------------");
         log.warn("---------------------------------------------------------------------------------------------------");
@@ -485,14 +302,14 @@ public class ClassTransformer implements IClassTransformer {
         }
     }
 
-    private MethodNode getMethod(ClassNode node, String name) {
+    protected MethodNode getMethod(ClassNode node, String name) {
         for (MethodNode m : node.methods)
             if (m.name.equals(name))
                 return m;
         return null;
     }
 
-    private String getInsn(AbstractInsnNode ins) {
+    protected String getInsn(AbstractInsnNode ins) {
         if (ins instanceof MethodInsnNode) {
             MethodInsnNode n = (MethodInsnNode) ins;
             return (n.owner + "," + n.name + "," + n.desc);
@@ -508,8 +325,7 @@ public class ClassTransformer implements IClassTransformer {
         return (ins.getOpcode() + ":" + ins.getClass().getSimpleName());
     }
 
-    @SuppressWarnings("unused")
-    private void showMethod(ClassNode classNode, String methodName) {
+    protected void showMethod(ClassNode classNode, String methodName) {
         log.warn("---------------------------------------------------------------------------------------------------");
         log.warn("Showing Method " + methodName + "----------------------------------------------------------------------------------");
         log.warn("---------------------------------------------------------------------------------------------------");
